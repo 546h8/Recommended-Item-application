804ad6cb3dfa988394c29c551e2e2934be53c6b5
546h8 <InforAPenny.InForAPound.Raki@gamil.com>
2020-07-24 12:22:40 +0900

    initial

--- db/migrate/20200722060203_create_items.rb
+++ db/migrate/20200722060203_create_items.rb
@@ -7,7 +7,6 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.float :rating,null: false
       t.string :price,null: false
       t.references :category, null:false, foreign_key: true
-
       t.timestamps
     end
   end
